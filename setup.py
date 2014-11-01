e4106da4bb988c7e8125c488b40f0a2356234296
Moss Collum <moss.collum@gmail.com>
2014-11-01 18:34:54 -0400

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nanogenmo',
+      version='1.0',
+      description="Moss's NaNoGenMo Novel",
+      author='Moss Collum',
+      author_email='moss@makingcodespeak.com',
+      packages=['nanogenmo'],
+      requires=['pytest'],
+     )
