99da110a90a31fd0a32520de970717ec9fdd49ed
Moss Collum <moss.collum@gmail.com>
2014-11-01 18:43:47 -0400

    A kind of declaration of intent.

--- nanogenmo/__init__.py
+++ nanogenmo/__init__.py
@@ -1,1 +1,2 @@
-__author__ = 'moss'
+def meatify(text):
+    return ""
---
+++ nanogenmo/tests/test_integration.py
@@ -0,0 +1,20 @@
+import nanogenmo
+
+SOURCE_TEXT = """
+Once there was a man.  He had a hat.  He went to a party.  He was very
+bored!  When the party was over he went home and ate cake.  It was
+delicious!  The cake was so filling that he went to sleep.
+
+When he woke up, he had to go buy some tools.  It was boring too.
+"""
+
+AFTER_MEATIFYING = """
+Once there was a meat.  He had a meat.  He went to a meat.  He was very
+bored!  When the meat was over he went home and ate meat.  It was
+delicious!  The meat was so filling that he went to sleep.
+
+When he woke up, he had to go buy some meats.  It was boring too.
+"""
+
+def test_integration():
+    assert nanogenmo.meatify(SOURCE_TEXT) == AFTER_MEATIFYING
