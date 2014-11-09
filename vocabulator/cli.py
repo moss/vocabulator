a4f39113bb1e28134ff85056700e71e889c5218b
Moss Collum <moss.collum@gmail.com>
2014-11-09 18:08:22 -0500

    Refactor CLI

--- vocabulator/cli.py
+++ vocabulator/cli.py
@@ -1,29 +1,18 @@
+"""
+vocabulator - Create hybrid novels.
+
+Usage:
+  vocabulator (--nouns-from <noun-text> | --nouns <nouns>) <target-text>
+"""
 from itertools import cycle
 from docopt import docopt
 from vocabulator.documents import Document
 
 
 class Vocabulator:
-    def __init__(self):
-        self.document = None
-        self.nouns = None
-
-    def use_document(self, document_file):
-        self.document = Document.from_file(document_file)
-
-    def use_document_text(self, document_text):
-        self.document = Document(document_text)
-
-    def use_nouns(self, nouns):
+    def __init__(self, document=None, nouns=None):
+        self.document = document
         self.nouns = nouns
-
-    def use_nouns_from(self, noun_text_file):
-        noun_document = Document.from_file(noun_text_file)
-        self.nouns = self.nouns_from(noun_document)
-
-    def use_nouns_from_text(self, noun_text):
-        noun_document = Document(noun_text)
-        self.nouns = self.nouns_from(noun_document)
 
     def vocabulate(self):
         nouns = iter(cycle(self.nouns))
@@ -32,23 +21,29 @@ class Vocabulator:
                 chunk.replace_with(next(nouns))
         return str(self.document)
 
-    @staticmethod
-    def nouns_from(document):
-        return [c.singular_form() for c in document.chunks if c.is_noun()]
+
+def nouns_from(document):
+    return [c.singular_form() for c in document.chunks if c.is_noun()]
+
+
+class VocabulatorOptions:
+    def __init__(self, argv=None):
+        self.options = docopt(__doc__, argv=argv)
+
+    def document(self):
+        return Document.from_file(self.options['<target-text>'])
+
+    def nouns(self):
+        if self.options['--nouns-from']:
+            return nouns_from(Document.from_file(self.options['<noun-text>']))
+        elif self.options['--nouns']:
+            return self.options['<nouns>'].split(',')
+
+    def vocabulator(self):
+        return Vocabulator(document=self.document(), nouns=self.nouns())
 
 
 def vocabulator():
-    """
-    Create hybrid novels.
-
-    Usage:
-      vocabulator (--nouns-from <noun-text> | --nouns <nouns>) <target-text>
-    """
-    opt = docopt(vocabulator.__doc__)
-    v = Vocabulator()
-    v.use_document(opt['<target-text>'])
-    if opt['--nouns-from']:
-        v.use_nouns_from(opt['<noun-text>'])
-    elif opt['--nouns']:
-        v.use_nouns(opt['<nouns>'].split(','))
+    opt = VocabulatorOptions()
+    v = opt.vocabulator()
     print(v.vocabulate())
--- vocabulator/tests/test_vocabulator.py
+++ vocabulator/tests/test_vocabulator.py
@@ -1,4 +1,5 @@
-from vocabulator.cli import Vocabulator
+from vocabulator.cli import Vocabulator, nouns_from
+from vocabulator.documents import Document
 
 SOURCE_TEXT = """
 Once there was a man.  He had a hat.  He went to a party.  He was very
@@ -15,9 +16,7 @@ a badger with purple spines!
 
 
 def test_meatify():
-    v = Vocabulator()
-    v.use_document_text(SOURCE_TEXT)
-    v.use_nouns(['meat'])
+    v = Vocabulator(document=Document(SOURCE_TEXT), nouns=['meat'])
     assert v.vocabulate() == """
 Once there was a meat.  He had a meat.  He went to a meat.  He was very
 bored!  When the meat was over he went meat and ate meat.  It was
@@ -28,9 +27,7 @@ When he woke up, he had to go buy some meat.  It was boring too.
 
 
 def test_swap_nouns():
-    v = Vocabulator()
-    v.use_document_text(SOURCE_TEXT)
-    v.use_nouns_from_text(ANOTHER_SOURCE)
+    v = Vocabulator(document=Document(SOURCE_TEXT), nouns=nouns_from(Document(ANOTHER_SOURCE)))
     assert v.vocabulate() == """
 Once there was a scientist.  He had a day.  He went to a wood.  He was very
 bored!  When the badger was over he went spine and ate scientist.  It was
@@ -40,9 +37,7 @@ When he woke up, he had to go buy some woods.  It was boring too.
 """
 
 def test_swap_nouns_the_other_way():
-    v = Vocabulator()
-    v.use_document_text(ANOTHER_SOURCE)
-    v.use_nouns_from_text(SOURCE_TEXT)
+    v = Vocabulator(document=Document(ANOTHER_SOURCE), nouns=nouns_from(Document(SOURCE_TEXT)))
     assert v.vocabulate() == """
 Winston was a man. One hat he ventured into the parties. He found
 a party with purple homes!
