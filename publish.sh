13d342784f5de865662d7306a5a4a4229c7c9b15
Moss Collum <moss.collum@gmail.com>
2014-11-15 18:21:08 -0500

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+export LANG=en_US.UTF-8
+if [ $VIRTUALENV ]; then
+    source $VIRTUALENV/bin/activate
+fi
+python --version
+vocabulator --nouns-from pg5200.txt pg1342.txt > a-dream-of-large-vermin.txt
+scp a-dream-of-large-vermin.txt $TARGET
